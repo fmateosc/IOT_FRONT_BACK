914f3dc1620a2a295c4f8bfe005bc5ccf3382bc1
fmateosc <fmateosc@gmail.com>
2026-09-25 17:05:21 +0100

    Configurar CORS y el puerto HTTP en el archivo main

---
+++ src/config/cors.ts
@@ -0,0 +1,9 @@
+// src/config/cors.ts
+
+import { CorsOptions } from '@nestjs/common/interfaces/external/cors-options.interface';
+
+export const CORS: CorsOptions = {
+  origin: true,
+  methods: 'GET,HEAD,PUT,PATCH,DELETE,OPTIONS',
+  credentials: true,
+};
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
+// src/main.ts
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
+import { Logger } from '@nestjs/common';
+import { CORS } from './config/cors.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  const logger = new Logger('Bootstrap');
+
+  // Puertos
+  const HTTP_PORT = process.env.HTTP_PORT || 3000;
+  const HTTPS_PORT = process.env.HTTPS_PORT || 3443;
+
+  // cors
+  app.enableCors(CORS);
+
+  // prefix
+  app.setGlobalPrefix('api/v1');
+
+  // http
+  app.listen(HTTP_PORT, ()=>{
+    logger.log(`🚀 HTTP server inicializado en el puerto: ${HTTP_PORT}`);
+  })
 }
 await bootstrap();
